e57566840ea5acb8d094269920eda8d67b590287
ih4t3youall <sourcesistemas@gmail.com>
2015-10-14 11:34:48 -0300

    se agre hibernate, se agrega al pom el oracle connector, se agrega en el proyecto el oracle connector jar

---
+++ src/main/java/ar/com/sgt/dao/PersonaDAO.java
@@ -0,0 +1,38 @@
+package ar.com.sgt.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import ar.com.sgt.dominio.Persona;
+
+public class PersonaDAO {
+
+	
+	 private SessionFactory sessionFactory;
+	 
+	    public void setSessionFactory(SessionFactory sessionFactory) {
+	        this.sessionFactory = sessionFactory;
+	    }
+	     
+	    
+	    public void save(Persona p) {
+	        Session session = this.sessionFactory.openSession();
+	        Transaction tx = session.beginTransaction();
+	        session.persist(p);
+	        tx.commit();
+	        session.close();
+	    }
+	 
+	    @SuppressWarnings("unchecked")
+	    
+	    public List<Persona> list() {
+	        Session session = this.sessionFactory.openSession();
+	        List<Persona> personList = session.createQuery("from Person").list();
+	        session.close();
+	        return personList;
+	    }
+	
+}
---
+++ src/main/java/ar/com/sgt/dominio/Persona.java
@@ -0,0 +1,52 @@
+package ar.com.sgt.dominio;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "persona")
+public class Persona {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String name;
+
+	private String conuntry;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getConuntry() {
+		return conuntry;
+	}
+
+	public void setConuntry(String conuntry) {
+		this.conuntry = conuntry;
+	}
+	
+	@Override
+	public String toString(){
+		return id+name+conuntry;
+		
+	}
+
+}
