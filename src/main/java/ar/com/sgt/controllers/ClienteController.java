fb30c781cd7af938675d70deb384179417a4dfa2
mauro.damian.plaquin <mauro.damian.plaquin>
2015-11-19 09:58:37 -0300

    add clienteController

--- src/main/java/ar/com/sgt/controllers/ClienteController.java
+++ src/main/java/ar/com/sgt/controllers/ClienteController.java
@@ -2,83 +2,97 @@ package ar.com.sgt.controllers;
 
 import java.util.HashMap;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import ar.com.sgt.dto.ClienteDTO;
-import ar.com.sgt.dto.PinDTO;
+import ar.com.sgt.persistence.entity.Cliente;
+import ar.com.sgt.persistence.entity.ClienteDTO;
+import ar.com.sgt.persistence.entity.PinDTO;
+import ar.com.sgt.services.IClienteService;
 import ar.com.sgt.services.UserService;
 import ar.com.sgt.services.impl.MenuService;
 
 @Controller
 public class ClienteController {
 
-	@Autowired
-	private MenuService menuService;
-	@Autowired
-	private UserService userService;
+    private MenuService menuService;
 
-	@RequestMapping("seleccionarUsuario.htm")
-	public ModelAndView seleccionarUsuario() {
+    private UserService userService;
 
-		ModelAndView mav = new ModelAndView("recepcion/seleccionTipoCliente");
+    private IClienteService clienteService;
 
-		return mav;
+    @RequestMapping("seleccionarUsuario")
+    public ModelAndView seleccionarUsuario() {
 
-	}
+	ModelAndView mav = new ModelAndView("seleccionTipoCliente");
 
-	@RequestMapping("usuarioCliente.htm")
-	public ModelAndView usuarioCliente() {
+	return mav;
 
-		HashMap<String, String> menuCliente = menuService.menuCliente();
-		ModelAndView mav = new ModelAndView("recepcion/ingresoCliente");
-		mav.addObject("pinDTO",new PinDTO());
-		
-		return mav;
+    }
 
-	}
-	
-	@RequestMapping("verificarPin.htm")
-	public ModelAndView verificarPin(PinDTO pinDTO){
-		ClienteDTO clienteDTO = userService.getUserByPin(pinDTO.getPin());
-		
-		ModelAndView mav = new ModelAndView("recepcion/opciones");
-		HashMap<String, String> menuCliente = menuService.menuCliente();
-		mav.addObject("opciones",menuCliente);
-		mav.addObject("clienteDTO",clienteDTO);
-		
-		
-		return mav;
-	}
+    @RequestMapping("usuarioCliente")
+    public ModelAndView usuarioCliente() {
 
-	@RequestMapping("usuarioNoCliente.htm")
-	public ModelAndView usuarioNoCliente() {
-		ModelAndView mav = new ModelAndView("recepcion/opciones");
-		HashMap<String, String> menuNoCliente = menuService.menuNoCliente();
-		mav.addObject("opciones",menuNoCliente);
-		return mav;
+	HashMap<String, String> menuCliente = menuService.menuCliente();
+	ModelAndView mav = new ModelAndView("ingresoCliente");
+	mav.addObject("pinDTO", new PinDTO());
 
-	}
+	return mav;
 
-	public MenuService getMenuService() {
-		return menuService;
-	}
+    }
 
-	public void setMenuService(MenuService menuService) {
-		this.menuService = menuService;
-	}
+    @RequestMapping("verificarPin")
+    public ModelAndView verificarPin(PinDTO pinDTO) {
+	ClienteDTO clienteDTO = userService.getUserByPin(pinDTO.getPin());
+	Cliente cliente = clienteService.getClienteByPin(pinDTO.getPin());
 
-	public UserService getUserService() {
-		return userService;
-	}
+	ModelAndView mav = new ModelAndView("opciones");
+	HashMap<String, String> menuCliente = menuService.menuCliente();
+	mav.addObject("opciones", menuCliente);
+	mav.addObject("clienteDTO", cliente);
 
-	public void setUserService(UserService userService) {
-		this.userService = userService;
-	}
-	
-	
-	
+	return mav;
+    }
+
+    @RequestMapping("usuarioNoCliente")
+    public ModelAndView usuarioNoCliente() {
+	ModelAndView mav = new ModelAndView("opciones");
+	HashMap<String, String> menuNoCliente = menuService.menuNoCliente();
+	mav.addObject("opciones", menuNoCliente);
+	return mav;
+
+    }
+
+    public MenuService getMenuService() {
+	return menuService;
+    }
+
+    public void setMenuService(MenuService menuService) {
+	this.menuService = menuService;
+    }
+
+    public UserService getUserService() {
+	return userService;
+    }
+
+    public void setUserService(UserService userService) {
+	this.userService = userService;
+    }
+
+    /**
+     * @return the clienteService
+     */
+    public IClienteService getClienteService() {
+	return clienteService;
+    }
+
+    /**
+     * @param clienteService
+     *            the clienteService to set
+     */
+    public void setClienteService(IClienteService clienteService) {
+	this.clienteService = clienteService;
+    }
 
 }
